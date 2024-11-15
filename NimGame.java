76712225addcdfc7454ad481e4628f3c9fdba529
Harveer-Saini <saini8304@mydusd.org>
2024-11-14 21:09:50 -0800

    Update NimGame.java

--- NimGame.java
+++ NimGame.java
@@ -5,36 +5,34 @@ public class NimGame {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Set up the initial pile of pieces
-        int piecesLeft = 20; // Number of pieces in the game
+        int piecesLeft = 20; // Number of pieces initially in the game
 
-        // Set AI to start
+        // Starting the AI 
         boolean aiTurn = true;
 
-        System.out.println("Welcome to the Modified Nim Game!");
-        System.out.println("We begin with " + piecesLeft + " pieces on the table.");
-        System.out.println("Each player must remove between 1 and half of the remaining pieces each turn.");
-        System.out.println("The player who takes the last piece loses.");
-        System.out.println("AI begins the game.\n");
+        System.out.println("Game of Nim!");
+        System.out.println("At the start of the game, there are " + piecesLeft + " pieces on the table.");
+        System.out.println("You must remove between 1 to half of the pieces left after each turn.");
+        System.out.println("Whoever takes the last piece will lose the game, and the AI will start.");
 
         while (piecesLeft > 0) {
             System.out.println("Pieces remaining: " + piecesLeft);
 
             if (aiTurn) {
                 // AI's move
-                int aiRemove = NimAI.calculateAIMove(piecesLeft);
+                int aiRemove = NimGameAI.calculateAIMove(piecesLeft);
                 piecesLeft -= aiRemove;
-                System.out.println("AI removes " + aiRemove + " piece(s).");
+                System.out.println("The AI removed " + aiRemove + " piece(s).");
             } else {
                 // Player's move
-                System.out.print("\nYour move. How many pieces will you take? ");
+                System.out.print("\nYour turn! How many pieces do you want to remove? ");
                 int maxTake = piecesLeft / 2;
                 if (maxTake < 1) {
                     maxTake = 1;
                 }
                 int playerRemove = input.nextInt();
                 while (playerRemove < 1 || playerRemove > maxTake) {
-                    System.out.print("Invalid choice. Please remove between 1 and " + maxTake + " pieces: ");
+                    System.out.print("That is an invalid number. You have to remove between 1 and " + maxTake + " pieces: ");
                     playerRemove = input.nextInt();
                 }
                 piecesLeft -= playerRemove;
@@ -42,9 +40,9 @@ public class NimGame {
 
             if (piecesLeft == 0) {
                 if (aiTurn) {
-                    System.out.println("\nAI takes the last piece and loses! You win!");
+                    System.out.println("\nThe AI took the last piece, so the AI lost. Congratulations, you win the Game of Nim!");
                 } else {
-                    System.out.println("\nYou took the last piece. You lose! AI wins!");
+                    System.out.println("\nYou took the last piece, so you lost the game. Better luck next time!");
                 }
                 break;
             }
